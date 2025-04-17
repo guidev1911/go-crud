9164597377528f20e0ff41b54b65bddd38cb3fa1
guidev1911 <guilhermereal1911@gmail.com>
2025-04-16 22:40:24 -0300

    conexão com db

---
+++ database/connection.go
@@ -0,0 +1,23 @@
+package database
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	dsn := "root:123456@tcp(127.0.0.1:3306)/go_crud?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Erro ao conectar com o banco:", err)
+	}
+
+	DB = db
+	fmt.Println("Conexão com MySQL realizada com sucesso!")
+}
